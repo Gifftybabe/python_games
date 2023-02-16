88d421c73ee408c2515a9051aa313c2d63cd91d7
Gifftybabe <ulokangozi@gmail.com>
2023-02-16 15:43:10 +0100

    more_updates

---
+++ Word_Replacement.py
@@ -0,0 +1,9 @@
+def replace_word():
+
+    str = "Hey buddies, I am Gift, and nice meeting y'all"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+
+replace_word()
